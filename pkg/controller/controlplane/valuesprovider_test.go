14cb0476d083a2db6634f67a0193b30a80b85d69
Johannes Liebermann <johannes@kinvolk.io>
2020-12-29 15:39:34 +0200

    Set facility name in CCM chart
    The Packet CCM allows specifying the Packet facility in two ways:
    Using an env var and using the Packet metadata service.
    
    When deploying a Packet shoot with a seed which runs outside of
    Packet, the CCM which runs in the seed is crashlooping because it
    can't communicate with the Packet metadata service to figure out the
    facility automatically.
    
    Since the Spec.Region key of the Shoot resource seems to be required,
    looks like we can always specify the facility explicitly. Doing so
    allows using non-Packet seeds and shouldn't have any effect for
    Packet seeds as far as I can tell.

--- pkg/controller/controlplane/valuesprovider_test.go
+++ pkg/controller/controlplane/valuesprovider_test.go
@@ -84,6 +84,7 @@ var _ = Describe("ValuesProvider", func() {
 							Enabled: true,
 						},
 					},
+					Region: "ewr1",
 				},
 			},
 		}
@@ -126,6 +127,7 @@ var _ = Describe("ValuesProvider", func() {
 					"checksum/secret-cloud-controller-manager": "3d791b164a808638da9a8df03924be2a41e34cd664e42231c00fe369e3588272",
 					"checksum/secret-cloudprovider":            "8bafb35ff1ac60275d62e1cbd495aceb511fb354f74a20f7d06ecb48b3a68432",
 				},
+				"facility": "ewr1",
 			},
 			"csi-packet": map[string]interface{}{
 				"replicas":          1,
