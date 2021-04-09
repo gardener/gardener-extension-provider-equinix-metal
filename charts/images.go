2e78eddb121226a4971574766fe787c85c95ea8d
Rafael Franzke <rafael.franzke@sap.com>
2021-04-09 18:08:24 +0200

    Replace packr with native go:embed

--- charts/images.go
+++ charts/images.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+// Copyright (c) 2021 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package v20 a kube-scheduler specific configuration for
-// 1.20 Kubernetes version. The API types used by the scheduler
-// are in the v1beta1 subpackage.
-package v20
+package charts
+
+import _ "embed"
+
+// ImagesYAML contains the content of the images.yaml file
+//go:embed images.yaml
+var ImagesYAML string
