gardener/gardener-extension-provider-packet

add rook-ceph support
Signed-off-by: Avi Deitcher <avi@deitcher.net>

pkg/apis/packet/v1alpha1/zz_generated.conversion.go
@@ -24,6 +24,7 @@ import (
 	unsafe "unsafe"
 
 	packet "github.com/gardener/gardener-extension-provider-packet/pkg/apis/packet"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	conversion "k8s.io/apimachinery/pkg/conversion"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 )
@@ -105,6 +106,16 @@ func RegisterConversions(s *runtime.Scheme) error {
 	}); err != nil {
 		return err
 	}
+	if err := s.AddGeneratedConversionFunc((*Persistence)(nil), (*packet.Persistence)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_v1alpha1_Persistence_To_packet_Persistence(a.(*Persistence), b.(*packet.Persistence), scope)
+	}); err != nil {
+		return err
+	}
+	if err := s.AddGeneratedConversionFunc((*packet.Persistence)(nil), (*Persistence)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_packet_Persistence_To_v1alpha1_Persistence(a.(*packet.Persistence), b.(*Persistence), scope)
+	}); err != nil {
+		return err
+	}
 	if err := s.AddGeneratedConversionFunc((*WorkerStatus)(nil), (*packet.WorkerStatus)(nil), func(a, b interface{}, scope conversion.Scope) error {
 		return Convert_v1alpha1_WorkerStatus_To_packet_WorkerStatus(a.(*WorkerStatus), b.(*packet.WorkerStatus), scope)
 	}); err != nil {
@@ -139,6 +150,15 @@ func Convert_packet_CloudProfileConfig_To_v1alpha1_CloudProfileConfig(in *packet
 }
 
 func autoConvert_v1alpha1_ControlPlaneConfig_To_packet_ControlPlaneConfig(in *ControlPlaneConfig, out *packet.ControlPlaneConfig, s conversion.Scope) error {
+	if in.Persistence != nil {
+		in, out := &in.Persistence, &out.Persistence
+		*out = new(packet.Persistence)
+		if err := Convert_v1alpha1_Persistence_To_packet_Persistence(*in, *out, s); err != nil {
+			return err
+		}
+	} else {
+		out.Persistence = nil
+	}
 	return nil
 }
 
@@ -148,6 +168,15 @@ func Convert_v1alpha1_ControlPlaneConfig_To_packet_ControlPlaneConfig(in *Contro
 }
 
 func autoConvert_packet_ControlPlaneConfig_To_v1alpha1_ControlPlaneConfig(in *packet.ControlPlaneConfig, out *ControlPlaneConfig, s conversion.Scope) error {
+	if in.Persistence != nil {
+		in, out := &in.Persistence, &out.Persistence
+		*out = new(Persistence)
+		if err := Convert_packet_Persistence_To_v1alpha1_Persistence(*in, *out, s); err != nil {
+			return err
+		}
+	} else {
+		out.Persistence = nil
+	}
 	return nil
 }
 
@@ -260,6 +289,30 @@ func autoConvert_packet_MachineImages_To_v1alpha1_MachineImages(in *packet.Machi
 // Convert_packet_MachineImages_To_v1alpha1_MachineImages is an autogenerated conversion function.
 func Convert_packet_MachineImages_To_v1alpha1_MachineImages(in *packet.MachineImages, out *MachineImages, s conversion.Scope) error {
 	return autoConvert_packet_MachineImages_To_v1alpha1_MachineImages(in, out, s)
+}
+
+func autoConvert_v1alpha1_Persistence_To_packet_Persistence(in *Persistence, out *packet.Persistence, s conversion.Scope) error {
+	if err := v1.Convert_bool_To_Pointer_bool(&in.Enabled, &out.Enabled, s); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Convert_v1alpha1_Persistence_To_packet_Persistence is an autogenerated conversion function.
+func Convert_v1alpha1_Persistence_To_packet_Persistence(in *Persistence, out *packet.Persistence, s conversion.Scope) error {
+	return autoConvert_v1alpha1_Persistence_To_packet_Persistence(in, out, s)
+}
+
+func autoConvert_packet_Persistence_To_v1alpha1_Persistence(in *packet.Persistence, out *Persistence, s conversion.Scope) error {
+	if err := v1.Convert_Pointer_bool_To_bool(&in.Enabled, &out.Enabled, s); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Convert_packet_Persistence_To_v1alpha1_Persistence is an autogenerated conversion function.
+func Convert_packet_Persistence_To_v1alpha1_Persistence(in *packet.Persistence, out *Persistence, s conversion.Scope) error {
+	return autoConvert_packet_Persistence_To_v1alpha1_Persistence(in, out, s)
 }
 
 func autoConvert_v1alpha1_WorkerStatus_To_packet_WorkerStatus(in *WorkerStatus, out *packet.WorkerStatus, s conversion.Scope) error {

pkg/controller/controlplane/valuesprovider_test.go
@@ -15,10 +15,13 @@
 package controlplane
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"io/ioutil"
 
 	apispacket "github.com/gardener/gardener-extension-provider-packet/pkg/apis/packet"
+	apispacketv1alpha1 "github.com/gardener/gardener-extension-provider-packet/pkg/apis/packet/v1alpha1"
 	"github.com/gardener/gardener-extension-provider-packet/pkg/packet"
 	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
 	mockclient "github.com/gardener/gardener/pkg/mock/controller-runtime/client"
@@ -32,6 +35,8 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
@@ -45,31 +50,22 @@ var _ = Describe("ValuesProvider", func() {
 	var (
 		ctrl *gomock.Controller
 
+		encoder runtime.Encoder
+		encode  = func(obj runtime.Object) []byte {
+			b := &bytes.Buffer{}
+			Expect(encoder.Encode(obj, b)).To(Succeed())
+
+			data, err := ioutil.ReadAll(b)
+			Expect(err).ToNot(HaveOccurred())
+
+			return data
+		}
+
 		// Build scheme
 		scheme = runtime.NewScheme()
 		_      = apispacket.AddToScheme(scheme)
 
-		cp = &extensionsv1alpha1.ControlPlane{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "control-plane",
-				Namespace: namespace,
-			},
-			Spec: extensionsv1alpha1.ControlPlaneSpec{
-				Region: "ewr1",
-				SecretRef: corev1.SecretReference{
-					Name:      v1beta1constants.SecretNameCloudProvider,
-					Namespace: namespace,
-				},
-				DefaultSpec: extensionsv1alpha1.DefaultSpec{
-					ProviderConfig: &runtime.RawExtension{
-						Raw: encode(&apispacket.ControlPlaneConfig{}),
-					},
-				},
-				InfrastructureProviderStatus: &runtime.RawExtension{
-					Raw: encode(&apispacket.InfrastructureStatus{}),
-				},
-			},
-		}
+		cp *extensionsv1alpha1.ControlPlane
 
 		cidr    = "10.250.0.0/19"
 		cluster = &extensionscontroller.Cluster{
@@ -127,15 +123,53 @@ var _ = Describe("ValuesProvider", func() {
 				},
 				"facility": "ewr1",
 			},
-			"metallb": map[string]interface{}{},
+			"metallb":   map[string]interface{}{},
+			"rook-ceph": map[string]interface{}{},
 		}
 
-		controlPlaneShootChartValues = map[string]interface{}{}
+		controlPlaneShootChartValues = map[string]interface{}{
+			"rook-ceph": map[string]interface{}{
+				"enabled": true,
+			},
+		}
 
 		logger = log.Log.WithName("test")
 	)
 
 	BeforeEach(func() {
+		codec := serializer.NewCodecFactory(scheme, serializer.EnableStrict)
+
+		info, found := runtime.SerializerInfoForMediaType(codec.SupportedMediaTypes(), runtime.ContentTypeJSON)
+		Expect(found).To(BeTrue(), "should be able to decode")
+
+		encoder = codec.EncoderForVersion(info.Serializer, apispacketv1alpha1.SchemeGroupVersion)
+
+		cp = &extensionsv1alpha1.ControlPlane{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "control-plane",
+				Namespace: namespace,
+			},
+			Spec: extensionsv1alpha1.ControlPlaneSpec{
+				Region: "EWR1",
+				SecretRef: corev1.SecretReference{
+					Name:      v1beta1constants.SecretNameCloudProvider,
+					Namespace: namespace,
+				},
+				DefaultSpec: extensionsv1alpha1.DefaultSpec{
+					ProviderConfig: &runtime.RawExtension{
+						Raw: encode(&apispacket.ControlPlaneConfig{
+							Persistence: &apispacket.Persistence{
+								Enabled: pointer.BoolPtr(true),
+							},
+						}),
+					},
+				},
+				InfrastructureProviderStatus: &runtime.RawExtension{
+					Raw: encode(&apispacket.InfrastructureStatus{}),
+				},
+			},
+		}
+
 		ctrl = gomock.NewController(GinkgoT())
 	})
 
@@ -185,8 +219,96 @@ var _ = Describe("ValuesProvider", func() {
 
 			// Create valuesProvider
 			vp := NewValuesProvider(logger)
-			err := vp.(inject.Client).InjectClient(client)
+			err := vp.(inject.Scheme).InjectScheme(scheme)
 			Expect(err).NotTo(HaveOccurred())
+			err = vp.(inject.Client).InjectClient(client)
+			Expect(err).NotTo(HaveOccurred())
+
+			// Call GetControlPlaneChartValues method and check the result
+			values, err := vp.GetControlPlaneShootChartValues(context.TODO(), cp, cluster, checksums)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(values).To(Equal(controlPlaneShootChartValues))
+		})
+	})
+
+	Describe("#GetControlPlaneShootChartValues", func() {
+		It("enables storage when persistance is enabled in configuration", func() {
+			// Create mock client
+			client := mockclient.NewMockClient(ctrl)
+			client.EXPECT().Get(context.TODO(), cpSecretKey, &corev1.Secret{}).DoAndReturn(clientGet(cpSecret))
+
+			// Create valuesProvider
+			vp := NewValuesProvider(logger)
+			err := vp.(inject.Scheme).InjectScheme(scheme)
+			Expect(err).NotTo(HaveOccurred())
+			err = vp.(inject.Client).InjectClient(client)
+			Expect(err).NotTo(HaveOccurred())
+
+			cp.Spec.DefaultSpec.ProviderConfig.Raw = encode(&apispacket.ControlPlaneConfig{
+				Persistence: &apispacket.Persistence{
+					Enabled: pointer.BoolPtr(true),
+				},
+			})
+
+			controlPlaneShootChartValues["rook-ceph"] = map[string]interface{}{
+				"enabled": true,
+			}
+
+			// Call GetControlPlaneChartValues method and check the result
+			values, err := vp.GetControlPlaneShootChartValues(context.TODO(), cp, cluster, checksums)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(values).To(Equal(controlPlaneShootChartValues))
+		})
+	})
+
+	Describe("#GetControlPlaneShootChartValues", func() {
+		It("disables storage when persistance is disabled in configuration", func() {
+			// Create mock client
+			client := mockclient.NewMockClient(ctrl)
+			client.EXPECT().Get(context.TODO(), cpSecretKey, &corev1.Secret{}).DoAndReturn(clientGet(cpSecret))
+
+			// Create valuesProvider
+			vp := NewValuesProvider(logger)
+			err := vp.(inject.Scheme).InjectScheme(scheme)
+			Expect(err).NotTo(HaveOccurred())
+			err = vp.(inject.Client).InjectClient(client)
+			Expect(err).NotTo(HaveOccurred())
+
+			cp.Spec.DefaultSpec.ProviderConfig.Raw = encode(&apispacket.ControlPlaneConfig{
+				Persistence: &apispacket.Persistence{
+					Enabled: pointer.BoolPtr(false),
+				},
+			})
+
+			controlPlaneShootChartValues["rook-ceph"] = map[string]interface{}{
+				"enabled": false,
+			}
+
+			// Call GetControlPlaneChartValues method and check the result
+			values, err := vp.GetControlPlaneShootChartValues(context.TODO(), cp, cluster, checksums)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(values).To(Equal(controlPlaneShootChartValues))
+		})
+	})
+
+	Describe("#GetControlPlaneShootChartValues", func() {
+		It("disables storage when persistance is not specified in configuration", func() {
+			// Create mock client
+			client := mockclient.NewMockClient(ctrl)
+			client.EXPECT().Get(context.TODO(), cpSecretKey, &corev1.Secret{}).DoAndReturn(clientGet(cpSecret))
+
+			// Create valuesProvider
+			vp := NewValuesProvider(logger)
+			err := vp.(inject.Scheme).InjectScheme(scheme)
+			Expect(err).NotTo(HaveOccurred())
+			err = vp.(inject.Client).InjectClient(client)
+			Expect(err).NotTo(HaveOccurred())
+
+			cp.Spec.DefaultSpec.ProviderConfig.Raw = encode(&apispacket.ControlPlaneConfig{})
+
+			controlPlaneShootChartValues["rook-ceph"] = map[string]interface{}{
+				"enabled": false,
+			}
 
 			// Call GetControlPlaneChartValues method and check the result
 			values, err := vp.GetControlPlaneShootChartValues(context.TODO(), cp, cluster, checksums)

gardener/gardener-extension-provider-equinix-metal

pkg/apis/equinixmetal/v1alpha1/zz_generated.conversion.go
@@ -105,6 +105,16 @@ func RegisterConversions(s *runtime.Scheme) error {
 	}); err != nil {
 		return err
 	}
+	if err := s.AddGeneratedConversionFunc((*Persistence)(nil), (*equinixmetal.Persistence)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_v1alpha1_Persistence_To_equinixmetal_Persistence(a.(*Persistence), b.(*equinixmetal.Persistence), scope)
+	}); err != nil {
+		return err
+	}
+	if err := s.AddGeneratedConversionFunc((*equinixmetal.Persistence)(nil), (*Persistence)(nil), func(a, b interface{}, scope conversion.Scope) error {
+		return Convert_equinixmetal_Persistence_To_v1alpha1_Persistence(a.(*equinixmetal.Persistence), b.(*Persistence), scope)
+	}); err != nil {
+		return err
+	}
 	if err := s.AddGeneratedConversionFunc((*WorkerConfig)(nil), (*equinixmetal.WorkerConfig)(nil), func(a, b interface{}, scope conversion.Scope) error {
 		return Convert_v1alpha1_WorkerConfig_To_equinixmetal_WorkerConfig(a.(*WorkerConfig), b.(*equinixmetal.WorkerConfig), scope)
 	}); err != nil {
@@ -149,6 +159,7 @@ func Convert_equinixmetal_CloudProfileConfig_To_v1alpha1_CloudProfileConfig(in *
 }
 
 func autoConvert_v1alpha1_ControlPlaneConfig_To_equinixmetal_ControlPlaneConfig(in *ControlPlaneConfig, out *equinixmetal.ControlPlaneConfig, s conversion.Scope) error {
+	out.Persistence = (*equinixmetal.Persistence)(unsafe.Pointer(in.Persistence))
 	return nil
 }
 
@@ -158,6 +169,7 @@ func Convert_v1alpha1_ControlPlaneConfig_To_equinixmetal_ControlPlaneConfig(in *
 }
 
 func autoConvert_equinixmetal_ControlPlaneConfig_To_v1alpha1_ControlPlaneConfig(in *equinixmetal.ControlPlaneConfig, out *ControlPlaneConfig, s conversion.Scope) error {
+	out.Persistence = (*Persistence)(unsafe.Pointer(in.Persistence))
 	return nil
 }
 
@@ -270,6 +282,26 @@ func autoConvert_equinixmetal_MachineImages_To_v1alpha1_MachineImages(in *equini
 // Convert_equinixmetal_MachineImages_To_v1alpha1_MachineImages is an autogenerated conversion function.
 func Convert_equinixmetal_MachineImages_To_v1alpha1_MachineImages(in *equinixmetal.MachineImages, out *MachineImages, s conversion.Scope) error {
 	return autoConvert_equinixmetal_MachineImages_To_v1alpha1_MachineImages(in, out, s)
+}
+
+func autoConvert_v1alpha1_Persistence_To_equinixmetal_Persistence(in *Persistence, out *equinixmetal.Persistence, s conversion.Scope) error {
+	out.Enabled = (*bool)(unsafe.Pointer(in.Enabled))
+	return nil
+}
+
+// Convert_v1alpha1_Persistence_To_equinixmetal_Persistence is an autogenerated conversion function.
+func Convert_v1alpha1_Persistence_To_equinixmetal_Persistence(in *Persistence, out *equinixmetal.Persistence, s conversion.Scope) error {
+	return autoConvert_v1alpha1_Persistence_To_equinixmetal_Persistence(in, out, s)
+}
+
+func autoConvert_equinixmetal_Persistence_To_v1alpha1_Persistence(in *equinixmetal.Persistence, out *Persistence, s conversion.Scope) error {
+	out.Enabled = (*bool)(unsafe.Pointer(in.Enabled))
+	return nil
+}
+
+// Convert_equinixmetal_Persistence_To_v1alpha1_Persistence is an autogenerated conversion function.
+func Convert_equinixmetal_Persistence_To_v1alpha1_Persistence(in *equinixmetal.Persistence, out *Persistence, s conversion.Scope) error {
+	return autoConvert_equinixmetal_Persistence_To_v1alpha1_Persistence(in, out, s)
 }
 
 func autoConvert_v1alpha1_WorkerConfig_To_equinixmetal_WorkerConfig(in *WorkerConfig, out *equinixmetal.WorkerConfig, s conversion.Scope) error {

pkg/controller/controlplane/valuesprovider_test.go
@@ -33,6 +33,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/utils/pointer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/inject"
@@ -100,10 +101,15 @@ var _ = Describe("ValuesProvider", func() {
 				},
 				"metro": "ny",
 			},
-			"metallb": map[string]interface{}{},
+			"metallb":   map[string]interface{}{},
+			"rook-ceph": map[string]interface{}{},
 		}
 
-		controlPlaneShootChartValues = map[string]interface{}{}
+		controlPlaneShootChartValues = map[string]interface{}{
+			"rook-ceph": map[string]interface{}{
+				"enabled": true,
+			},
+		}
 
 		logger = log.Log.WithName("test")
 	)
@@ -127,7 +133,11 @@ var _ = Describe("ValuesProvider", func() {
 				},
 				DefaultSpec: extensionsv1alpha1.DefaultSpec{
 					ProviderConfig: &runtime.RawExtension{
-						Raw: encode(&api.ControlPlaneConfig{}),
+						Raw: encode(&api.ControlPlaneConfig{
+							Persistence: &api.Persistence{
+								Enabled: pointer.BoolPtr(true),
+							},
+						}),
 					},
 				},
 				InfrastructureProviderStatus: &runtime.RawExtension{
@@ -188,6 +198,92 @@ var _ = Describe("ValuesProvider", func() {
 			Expect(err).NotTo(HaveOccurred())
 			err = vp.(inject.Client).InjectClient(client)
 			Expect(err).NotTo(HaveOccurred())
+
+			// Call GetControlPlaneChartValues method and check the result
+			values, err := vp.GetControlPlaneShootChartValues(context.TODO(), cp, cluster, checksums)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(values).To(Equal(controlPlaneShootChartValues))
+		})
+	})
+
+	Describe("#GetControlPlaneShootChartValues", func() {
+		It("enables storage when persistance is enabled in configuration", func() {
+			// Create mock client
+			client := mockclient.NewMockClient(ctrl)
+			client.EXPECT().Get(context.TODO(), cpSecretKey, &corev1.Secret{}).DoAndReturn(clientGet(cpSecret))
+
+			// Create valuesProvider
+			vp := NewValuesProvider(logger)
+			err := vp.(inject.Scheme).InjectScheme(scheme)
+			Expect(err).NotTo(HaveOccurred())
+			err = vp.(inject.Client).InjectClient(client)
+			Expect(err).NotTo(HaveOccurred())
+
+			cp.Spec.DefaultSpec.ProviderConfig.Raw = encode(&api.ControlPlaneConfig{
+				Persistence: &api.Persistence{
+					Enabled: pointer.BoolPtr(true),
+				},
+			})
+
+			controlPlaneShootChartValues["rook-ceph"] = map[string]interface{}{
+				"enabled": true,
+			}
+
+			// Call GetControlPlaneChartValues method and check the result
+			values, err := vp.GetControlPlaneShootChartValues(context.TODO(), cp, cluster, checksums)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(values).To(Equal(controlPlaneShootChartValues))
+		})
+	})
+
+	Describe("#GetControlPlaneShootChartValues", func() {
+		It("disables storage when persistance is disabled in configuration", func() {
+			// Create mock client
+			client := mockclient.NewMockClient(ctrl)
+			client.EXPECT().Get(context.TODO(), cpSecretKey, &corev1.Secret{}).DoAndReturn(clientGet(cpSecret))
+
+			// Create valuesProvider
+			vp := NewValuesProvider(logger)
+			err := vp.(inject.Scheme).InjectScheme(scheme)
+			Expect(err).NotTo(HaveOccurred())
+			err = vp.(inject.Client).InjectClient(client)
+			Expect(err).NotTo(HaveOccurred())
+
+			cp.Spec.DefaultSpec.ProviderConfig.Raw = encode(&api.ControlPlaneConfig{
+				Persistence: &api.Persistence{
+					Enabled: pointer.BoolPtr(false),
+				},
+			})
+
+			controlPlaneShootChartValues["rook-ceph"] = map[string]interface{}{
+				"enabled": false,
+			}
+
+			// Call GetControlPlaneChartValues method and check the result
+			values, err := vp.GetControlPlaneShootChartValues(context.TODO(), cp, cluster, checksums)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(values).To(Equal(controlPlaneShootChartValues))
+		})
+	})
+
+	Describe("#GetControlPlaneShootChartValues", func() {
+		It("disables storage when persistance is not specified in configuration", func() {
+			// Create mock client
+			client := mockclient.NewMockClient(ctrl)
+			client.EXPECT().Get(context.TODO(), cpSecretKey, &corev1.Secret{}).DoAndReturn(clientGet(cpSecret))
+
+			// Create valuesProvider
+			vp := NewValuesProvider(logger)
+			err := vp.(inject.Scheme).InjectScheme(scheme)
+			Expect(err).NotTo(HaveOccurred())
+			err = vp.(inject.Client).InjectClient(client)
+			Expect(err).NotTo(HaveOccurred())
+
+			cp.Spec.DefaultSpec.ProviderConfig.Raw = encode(&api.ControlPlaneConfig{})
+
+			controlPlaneShootChartValues["rook-ceph"] = map[string]interface{}{
+				"enabled": false,
+			}
 
 			// Call GetControlPlaneChartValues method and check the result
 			values, err := vp.GetControlPlaneShootChartValues(context.TODO(), cp, cluster, checksums)