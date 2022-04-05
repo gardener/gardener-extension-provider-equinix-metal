e3eed3f16134c0b301adfe7cb604c8713b8bead5
Tim Ebert <timebertt@gmail.com>
2022-04-05 10:29:32 +0200

    Vendor gardener/gardener@v1.44 (#205)
    * make `make revendor` deterministic
    
    * Add tools to `make generate`
    
    * Upgrade gardener/gardener
    
    * Add replace directives for hvpa-controller
    
    * [automated] make revendor
    
    * [automated] make generate

--- vendor/k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1/doc.go
+++ vendor/k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The Kubernetes Authors.
+Copyright 2019 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,5 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package config contains libraries for initializing REST configs for talking to the Kubernetes API
-package config
+// +k8s:deepcopy-gen=package,register
+
+// Package v1 contains definitions of Vertical Pod Autoscaler related objects.
+// +groupName=autoscaling.k8s.io
+package v1
