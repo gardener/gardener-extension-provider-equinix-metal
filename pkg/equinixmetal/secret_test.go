6063e6ac515d620842ba502fc68bad40b60541d8
Shafeeque E S <shafeeque.e.s@sap.com>
2022-09-15 11:23:01 +0530

    Adapt client Get call with GetOption

--- pkg/equinixmetal/secret_test.go
+++ pkg/equinixmetal/secret_test.go
@@ -72,7 +72,7 @@ var _ = Describe("Secret", func() {
 				projectID = []byte("bar")
 			)
 
-			c.EXPECT().Get(ctx, kutil.Key(namespace, name), gomock.AssignableToTypeOf(&corev1.Secret{})).DoAndReturn(func(_ context.Context, _ client.ObjectKey, secret *corev1.Secret) error {
+			c.EXPECT().Get(ctx, kutil.Key(namespace, name), gomock.AssignableToTypeOf(&corev1.Secret{})).DoAndReturn(func(_ context.Context, _ client.ObjectKey, secret *corev1.Secret, _ ...client.GetOption) error {
 				secret.Data = map[string][]byte{
 					APIToken:  apiToken,
 					ProjectID: projectID,
