f8dad06c8fb1c624c522e4f375214241b4f77490
Shafeeque E S <shafeeque.e.s@sap.com>
2023-02-28 11:13:29 +0530

    Run `make generate`

--- pkg/apis/equinixmetal/v1alpha1/zz_generated.conversion.go
+++ pkg/apis/equinixmetal/v1alpha1/zz_generated.conversion.go
@@ -24,10 +24,9 @@ package v1alpha1
 import (
 	unsafe "unsafe"
 
+	equinixmetal "github.com/gardener/gardener-extension-provider-equinix-metal/pkg/apis/equinixmetal"
 	conversion "k8s.io/apimachinery/pkg/conversion"
 	runtime "k8s.io/apimachinery/pkg/runtime"
-
-	equinixmetal "github.com/gardener/gardener-extension-provider-equinix-metal/pkg/apis/equinixmetal"
 )
 
 func init() {
